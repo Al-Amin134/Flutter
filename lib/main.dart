a8d381b616341da51deff37968aef6a5e50da43e
Md.Al-Amin Babu <s2110676134@ru.ac.bd>
2024-04-29 19:37:12 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Hello Prithibi',
+              'Hello world',
             ),
             Text(
               '$_counter',
